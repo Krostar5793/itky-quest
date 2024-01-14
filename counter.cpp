7a7a551aa2bd75839ec0331fd6b4ccefe2d1f3ae
nashdev255 <nashdev255@gmail.com>
2024-01-14 20:01:35 +0900

    Update .yml

--- counter.cpp
+++ counter.cpp
@@ -2,7 +2,7 @@
 #include <filesystem>
 
 int main() {
-    const std::string path = "dot-works"; // ディレクトリパス
+    const std::string path = "dot-works";
     int count = 0;
 
     for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
