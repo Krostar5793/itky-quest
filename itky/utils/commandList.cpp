6dd7e63453f89388250e8320cb259268be85d7b1
nashdev255 <nashdev255@gmail.com>
2024-01-23 23:26:28 +0900

    Add commandList.cpp

---
+++ itky/utils/commandList.cpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+#include <map>
+
+const std::map<std::string, std::string> commands = {
+  {"help", "help"},
+  {"--help", "help"},
+  {"version", "help"},
+  {"--version", "help"}
+};
