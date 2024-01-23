f07a1a8ffb7a95cf5982a71fa758ea92f005e0ec
nashdev255 <nashdev255@gmail.com>
2024-01-23 23:56:58 +0900

    Update rest

--- itky/itky.cpp
+++ itky/itky.cpp
@@ -1,5 +1,22 @@
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <vector>
 
-int main() {
+#include "CommandManager.h"
+#include "UnknownCommandError.h"
+
+int main(int argc, char* argv[]) {
+  const int8_t numberOfParams = argc;
+  const std::vector<std::string> args(argv, argv + argc);
+
+  const CommandManager* commandManager(CommandManager::getInstance());
+
+  try {
+    commandManager->command(args[1]);
+  } catch ( const UnknownCommandError& e ) {
+    std::cout << e.what() << std::endl;
+    return -1;
+  }
   return 0;
 }
