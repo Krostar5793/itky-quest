8d2524d2e7d9709f3c975b34aee4fba657bc4667
nashdev255 <nashdev255@gmail.com>
2024-01-24 20:19:56 +0900

    rest

--- itky/itky.cpp
+++ itky/itky.cpp
@@ -7,16 +7,14 @@
 #include "UnknownCommandError.h"
 
 int main(int argc, char* argv[]) {
-  const int8_t numberOfParams = argc;
-  const std::vector<std::string> args(argv, argv + argc);
-
   const CommandManager* commandManager(CommandManager::getInstance());
 
   try {
-    commandManager->command(args[1]);
+    commandManager->command(argv[1]);
   } catch ( const UnknownCommandError& e ) {
     std::cout << e.what() << std::endl;
     return -1;
   }
+  
   return 0;
 }
