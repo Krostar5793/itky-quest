8a324cb402bc6da1d8a52c15ec1f2d864cff89d2
nashdev255 <nashdev255@gmail.com>
2024-01-24 22:37:21 +0900

    rest

--- itky-gui/src/app/page.tsx
+++ itky-gui/src/app/page.tsx
@@ -1,6 +1,11 @@
+import Header from '@/app/components/Header';
+import Footer from '@/app/components/Footer';
+
 export default function Home() {
   return (
     <>
+      <Header />
+      <Footer />
     </>
   );
 }
