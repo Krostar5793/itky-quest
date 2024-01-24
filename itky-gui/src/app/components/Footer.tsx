2a30efb8c2d0c91177d026fbd456c2f1bc34e630
nashdev255 <nashdev255@gmail.com>
2024-01-24 21:56:20 +0900

    Add Footer.tsx

---
+++ itky-gui/src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const Footer = () => {
+  return (
+    <footer className='flex h-16 items-center'>
+      <div className='flex w-full justify-between px-[8vw]'>
+        <span>{ new Date().getFullYear() } &copy; All Rights Reserved</span>
+        <div>
+          Build with <Link href={'https://github.com/nashdev255/'} target='blank'>nashdev255</Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
