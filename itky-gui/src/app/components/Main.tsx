140376557c4fdbbd6cbca7817f224bc96e2d47e3
nashdev255 <nashdev255@gmail.com>
2024-01-26 07:08:15 +0900

    Add Main

---
+++ itky-gui/src/app/components/Main.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import HeadLine from '@/app/components/HeadLine';
+
+const Main = () => {
+  return (
+    <main className='px-[8vw] py-4'>
+      <HeadLine />
+    </main>
+  );
+};
+
+export default Main;
--- itky-gui/src/app/page.tsx
+++ itky-gui/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Header from '@/app/components/Header';
-import HeadLine from '@/app/components/HeadLine';
+import Main from '@/app/components/Main';
 import Footer from '@/app/components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HeadLine />
+      <Main />
       <Footer />
     </>
   );
