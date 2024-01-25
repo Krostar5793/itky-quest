2bf6f8caf99f4930194d796725c2ee361d70eda8
nashdev255 <nashdev255@gmail.com>
2024-01-26 07:03:10 +0900

    Add HeadLine

---
+++ itky-gui/src/app/components/HeadLine.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const HeadLine = () => {
+  return (
+    <>
+      <h1 className='font-mono text-5xl font-bold'>Welcome, Developpers!</h1>
+    </>
+  );
+};
+
+export default HeadLine;
--- itky-gui/src/app/page.tsx
+++ itky-gui/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/app/components/Header';
+import HeadLine from '@/app/components/HeadLine';
 import Footer from '@/app/components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <HeadLine />
       <Footer />
     </>
   );
