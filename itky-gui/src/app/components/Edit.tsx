e73a05190179f0657b55eeebd6fa05e0aa7e86e3
nashdev255 <nashdev255@gmail.com>
2024-01-27 23:25:02 +0900

    Add form

--- itky-gui/src/app/components/Edit.tsx
+++ itky-gui/src/app/components/Edit.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Loading from '@/app/components/Loading';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -15,11 +16,12 @@ const style = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  height: '60vh',
-  width: '75vw',
+  height: '94vh',
+  width: '80vw',
   bgcolor: 'background.paper',
   boxShadow: 24,
-  p: 2,
+  px: 4,
+  py: 2
 };
 
 const closeButtonStyle = {
@@ -28,9 +30,14 @@ const closeButtonStyle = {
 };
 
 const Edit = () => {
+  const [loading, setLoading] = useState(false);
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const handleSubmit = () => {
+
+  };
 
   return (
     <>
@@ -46,28 +53,125 @@ const Edit = () => {
               <CloseIcon />
             </IconButton>
           </Box>
-          <form className='flex flex-col'>
-            <h2 className='text-xl'>編集する</h2>
-            <select>
-              <option value='Monsters'>Monsters</option>
-              <option value='Weapons'>Weapons</option>
-              <option value='Armors'>Armors</option>
-              <option value='Items'>Items</option>
-              <option value='Materials'>Materials</option>
-            </select>
-            <label>
-              <input type='number' />
-            </label>
-            <input type='text' />
-            <input type='text' />
-            <input type='checkbox' />
-            <select>
-              <option value='メニュー画面'>メニュー画面</option>
-              <option value='戦闘画面'>戦闘画面</option>
-              <option value='常時'>常時</option>
-              <option value='使用不可'>使用不可</option>
-            </select>
-            <button type='submit'>作成する</button>
+          <form onSubmit={handleSubmit} className='mt-8'>
+            {/* 編集対象の選択 */}
+            <div className='mb-5 flex space-x-2'>
+              <div>
+                <select
+                  className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                  id='editType'
+                >
+                  <option>Monsters</option>
+                  <option>Weapons</option>
+                  <option>Armors</option>
+                  <option>Items</option>
+                  <option>Materials</option>
+                </select>
+              </div>
+              <p className='py-2'>を追加する</p>
+            </div>
+            
+
+            {/* 名前の入力 */}
+            <div className='mb-5'>
+              <div className='mb-1 text-sm font-bold'>名前</div>
+              <input
+                type='text'
+                className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                placeholder='名前'
+                id='name'
+                required
+              />
+            </div>
+
+            {/* レア度の選択 */}
+            <div className='flex space-x-8'>
+              <div className='mb-5 flex-1'>
+                <div className='mb-1 text-sm font-bold'>レア度</div>
+                <select
+                  className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                  id='rarity'
+                >
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </select>
+              </div>
+
+              {/* 価格の入力 */}
+              <div className='mb-5 flex-1'>
+                <div className='mb-1 text-sm font-bold'>価格</div>
+                <input
+                  type='number'
+                  className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline'
+                  placeholder='価格'
+                  id='price'
+                />
+              </div>
+            </div>
+
+            {/* 使用可能時の選択 */}
+            <div className='mb-5'>
+              <div className='mb-1 text-sm font-bold'>使用可能時</div>
+              <select
+                className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                id='rarity'
+              >
+                <option>メニュー画面</option>
+                <option>戦闘画面</option>
+                <option>常時</option>
+                <option>使用不可</option>
+              </select>
+            </div>
+
+            {/* 説明の入力 */}
+            <div className='mb-5'>
+              <div className='mb-1 text-sm font-bold'>説明</div>
+              <textarea
+                className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                placeholder='説明'
+                id='content'
+              />
+            </div>
+
+            {/* 使用効果 */}
+            <div className='mb-5'>
+              <div className='mb-1 text-sm font-bold'>使用効果</div>
+              <textarea
+                className='w-full rounded-md border px-3 py-2 focus:border-sky-500 focus:outline-none'
+                placeholder='説明'
+                id='effect'
+              />
+            </div>
+
+            {/* 消費の有無 */}
+            <div className='mb-5 flex space-x-4'>
+              <div className='mb-1 text-sm font-bold'>消費の有無</div>
+              <div>
+                <input type='radio' id='consumable' name='isConsumable' value='consumable' />
+                <label htmlFor='consumable'>あり</label>
+              </div>
+              <div>
+                <input type='radio' id='unconsumable' name='isConsumable' value='unconsumable' />
+                <label htmlFor='unconsumable'>なし</label>
+              </div>
+            </div>
+
+            {/* 変更ボタン */}
+            <div className='mb-5'>
+              {loading ? (
+                <Loading />
+              ) : (
+                <button
+                  type='submit'
+                  className='w-full rounded-full bg-sky-400 p-2 text-sm font-bold text-white'
+                >
+                  変更
+                </button>
+              )}
+            </div>
           </form>
         </Box>
       </Modal>
