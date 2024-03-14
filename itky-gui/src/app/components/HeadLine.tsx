19a2e656fab3b93113f76fba0a9a63f604e52ee6
nashdev255 <nashdev255@gmail.com>
2024-03-15 00:53:13 +0900

    Adjust Headline btn hover scale and font-family

--- itky-gui/src/app/components/HeadLine.tsx
+++ itky-gui/src/app/components/HeadLine.tsx
@@ -7,10 +7,10 @@ const HeadLine = () => {
     <>
       <div className="mb-8 flex">
         <div className='flex-col space-y-4'>
-          <h1 className='font-mono text-4xl font-bold md:text-5xl'>Welcome, Developpers!</h1>
+          <h1 className='font-sanserif text-4xl font-bold drop-shadow-lg md:text-5xl'>Welcome, Developpers!</h1>
           <div className='flex justify-center'>
-            <Link href={'/dashboard/edit'} className='rounded-full bg-black px-4 py-2 text-white'>
-              編集する
+            <Link href={'/dashboard/edit'} className='rounded-full bg-black px-4 py-2 text-white shadow-lg hover:scale-110'>
+              <p className='font-bold'>編集する</p>
             </Link>
           </div>
         </div>
